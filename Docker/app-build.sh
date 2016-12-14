c837f2d6269b98dfca1f771fef43f44fe1df26a2
ozil87 <nguyenhaichaudt5@gmail.com>
2016-12-14 21:02:38 +0700

    Update app-build.sh

--- Docker/app-build.sh
+++ Docker/app-build.sh
@@ -2,5 +2,5 @@ cd ~ &&
 git clone https://github.com/ozil87/pyconjp-android.git && 
 cd pyconjp-android && 
 git checkout master &&
-GOOGLE_MAP_APIKEY_DEBUG='AIzaSyCCXbN6Cz1GwBVN0-Iwtv88AxMawmx30Z0' FABRIC_APIKEY='' ./gradlew clean assembleDebug
+GOOGLE_MAP_APIKEY_DEBUG='AIzaSyCCXbN6Cz1GwBVN0-Iwtv88AxMawmx30Z0' FABRIC_APIKEY='12430140ad5b86ad46425c2d201946ea7795cb42' ./gradlew clean assembleDebug
 ls
